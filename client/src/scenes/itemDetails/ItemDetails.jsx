952388301fdf8860501d3b88c99efa775967daa2
iamlydial <lydia.lavecchia@gmail.com>
2024-01-03 21:59:46 +0100

    started the item details view

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,5 +1,88 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import { shades } from "../../theme";
+import { addToCart } from "../../state";
+import { useParams } from "react-router-dom";
+import Item from "../../components/Item";
+import { Box, Typography } from "@mui/material";
+
 const ItemDetails = () => {
-  return <div>ItemDetails</div>;
+  const dispatch = useDispatch();
+  const { itemId } = useParams();
+  const [value, setValue] = useState("description");
+  const [count, setCount] = useState(1);
+  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItem() {
+    const item = await fetch(
+      `http://localhost:1337/api/items/${itemId}?populate=image`,
+      { method: "GET" }
+    );
+    const itemJson = await item.json();
+    setItem(itemJson.data);
+  }
+
+  async function getItems() {
+    const item = await fetch(
+      `http://localhost:1337/api/items/?populate=image`,
+      { method: "GET" }
+    );
+    const itemJson = await item.json();
+    setItem(itemJson.data);
+  }
+
+  useEffect(() => {
+    getItem();
+    getItems();
+  }, [itemId]);
+
+  return (
+    <Box width="80%" m="80px auto">
+      <Box display="flex" flexWrap="wrap" columnGap="40px">
+        {/* IMAGES */}
+        <Box flex="1 1 40%" mb="40px">
+          <img
+            alt={item?.name}
+            width="100%"
+            height="100%"
+            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            style={{ objectFit: "contain" }}
+          />
+        </Box>
+        {/* ACTIONS */}
+        <Box flex="1 1 50%" mb="40px">
+          <Box display="flex" justifyContent="space-between">
+            <Box>Home / Item</Box>
+            <Box>Prev Next</Box>
+          </Box>
+          <Box m="65px 0 25px 0">
+            <Typography variant="h3">{item?.attributes?.name}</Typography>
+            <Typography>{item?.attributes?.price}</Typography>
+            <Typography sx={{ mt: "20px" }}>
+              {item?.attributes?.description}
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" minHeight="50px">
+            <Box
+              display="flex"
+              alignItems="center"
+              border={`1.5px solid ${shades.neutral[300]}`}
+              mr="20px"
+              p="2px 5px"
+            ></Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ItemDetails;
